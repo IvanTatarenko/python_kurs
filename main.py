13585f3c4894de1c731e2fb797bb911cd7b66fc7
IvanTatarenko <vakummail@gmail.com>
2022-12-25 16:09:56 +0200

    homework 2

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print('Hello World!!!')
+
+a = 8
+b = 3
+
+
+print(a // b) #ділення без залишку
+print(a % b) #оримання залишку ділення
+print(a ** b) #ступень
+
+strint = 'Data'
+result = string * 4
+print(result)
