cba489408f7e972e7e61da464f63d5500403589b
IvanTatarenko <vakummail@gmail.com>
2023-01-06 17:12:41 +0200

    hw4 ex2

---
+++ hw4/2.py
@@ -0,0 +1,8 @@
+text_sum = 0
+text = ''
+while (text != 'end'):
+  text = input('Введіть число ')
+  if (text.isdecimal()): text_sum += int(text)
+  else: print('Нправильний символ. Вводьте тільки числа')
+
+print(f'Сума чисел: {text_sum}')
