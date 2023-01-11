828db446530020fb871825d73c183cf20651e750
IvanTatarenko <vakummail@gmail.com>
2023-01-11 22:26:16 +0200

    hw5 ex2

---
+++ hw5/2_2.py
@@ -0,0 +1,6 @@
+# sorted() - це функція, її можна:)
+def second_smallest(array):
+    return(sorted(array)[1])
+
+assert second_smallest([1, 2, 2, 3]) == 2
+assert second_smallest([-1, 10, -2, 2]) == -1
