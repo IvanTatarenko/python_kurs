9e4a8ebceed382e028fa7bd2bcbb9d1c64fbbc0a
IvanTatarenko <vakummail@gmail.com>
2023-01-13 18:53:15 +0200

    hw7 rdit1

--- hw7/1.py
+++ hw7/1.py
@@ -5,9 +5,6 @@ dividible_at = [2, 3, 4, 5, 6]
 if (1 <= start and start < 200 and end <= 200 and end > 1):
   dividible = {2: [], 3: [], 4: [], 5: [], 6: []}
   dividible = {key : [] for key in dividible_at}
-  # При використанні методу fromkeys при додаванні значень в масив, значення додаються у всі масиви наявні в словнику.
-  # Це метод класу і там силка на обєкт одна на всіх чи щось таке. 
-  # dividible = dict.fromkeys(dividible_at, [])
   
   for i in dividible_at:
     for j in range(start+1, end):
