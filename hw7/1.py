b570e04b37e90ced08d634628611adabe15bef3e
IvanTatarenko <vakummail@gmail.com>
2023-01-14 22:16:45 +0200

    edit

--- hw7/1.py
+++ hw7/1.py
@@ -3,7 +3,6 @@ end = int(input('Введіть число b: '))
 
 dividible_at = [2, 3, 4, 5, 6]
 if (1 <= start and start < 200 and end <= 200 and end > 1):
-  dividible = {2: [], 3: [], 4: [], 5: [], 6: []}
   dividible = {key : [] for key in dividible_at}
   
   for i in dividible_at:
@@ -11,7 +10,6 @@ if (1 <= start and start < 200 and end <= 200 and end > 1):
       if(j % i == 0):
         dividible[i].append(j)
   
-  print(dividible)
   for i in dividible:
     print(f'Dividible by {i}: {dividible[i][:10]}')
 else: print('Невірні числа')
--- hw7/exchange.py
+++ hw7/exchange.py
@@ -3,24 +3,64 @@ def cell_rate(buy_rate):
 
 #курс валют
 AUH_BUY = {
-    "EUR": 42.50,
-    "USD": 50.00,
-    "PLN": 30.00
+    'EUR': 42.50,
+    'USD': 50.00,
+    'PLN': 30.00
 }
-AUH_CELL = {
-    "EUR": cell_rate(AUH_BUY['EUR']),
-    "USD": cell_rate(AUH_BUY['USD']),
-    "PLN": cell_rate(AUH_BUY['PLN'])
+AUH_CELL = {key : cell_rate(AUH_BUY[key]) for key in AUH_BUY}
+
+#кількість купюр в касі
+bank = {
+  "USD": {
+    100 : 90, 
+    50: 20, 
+    10: 10, 
+    5:0, 
+    1:0
+  }
 }
 
+result_buy = {
+  'currency': 'USD',
+  'bank_sum': 0,
+  'currency_value': 0.0,
+  'auh_value': 0.0,
+  'remain': 0.0,
+  'offer_more': False,
+  'amount_more': 0.0,
+  'result_buy_more': 0.0,
+  'offer_less': False,
+  'enough_money': False
+}
+result_sell = {
+  'currency': 'USD',
+  'value': 0.00
+}
 
 #коеф. для підрахунку пропозиції купити більше або менше валюти
 ALIQUIT_INDEX = 50
 CLOSENESS_INTEREST = 0.05
 closeness_index = ALIQUIT_INDEX * CLOSENESS_INTEREST
 
-buy_calculations = {
-}
+#Визначення - чи потрібно пропонувати взяти більше або менше
+def offer():
+  remain_currency = result_buy['remain'] / AUH_BUY[result_buy['currency']]
+  if((remain_currency + result_buy['currency_value'] + closeness_index)//ALIQUIT_INDEX > result_buy['currency_value']//ALIQUIT_INDEX):
+    result_buy['result_buy_more'] = (result_buy['currency_value']//ALIQUIT_INDEX + 1.0) * ALIQUIT_INDEX
+    result_buy['amount_more'] = (result_buy['result_buy_more'] - result_buy['currency_value'])/AUH_BUY[result_buy['currency']]
+    result_buy['offer_more'] = True
+  elif((remain_currency + result_buy['currency_value'] - closeness_index)//ALIQUIT_INDEX < result_buy['currency_value']//ALIQUIT_INDEX):
+    result_buy['offer_less'] = True
+  
+#Підрахунок грошей в касі
+def bank_val():
+  bank_sum = 0
+  for i in bank[result_buy['currency']]:
+    bank_sum += (i * bank[result_buy['currency']][i])
+  result_buy['bank_sum'] = bank_sum
+  if(result_buy['currency_value'] < bank_sum):
+    result_buy['enough_money'] = True
+  
 #Вивід курсів валют
 print(f"{'':*^17}")
 print(f"*{'BUY':^5}{'':5}{'SELL':^5}*")
@@ -28,96 +68,92 @@ for i in AUH_BUY:
   print(f"*{AUH_BUY[i]:<6.2f}{i:^3}{AUH_CELL[i]:>6.2f}*")
 print(f"{'':*^17}")
 
+print(result_buy)
+
 operation_selection = input("Ви бажаєте купити чи продати долари?(купити/продати)")
 
-def buy():
-  buy_calculations['value'] = float(input("Введіть кількість гривень, яку ви хочете продати: "))
-  buy_calculations['result'] = buy_calculations['value'] // AUH_BUY['USD']
-  print(buy_calculations)
-  remain = buy_calculations['value'] % AUH_BUY['USD']
-  print(f"Ваша валюта {buy_calculations['result']:>9.2f} дол")
-  print(f"Ваша решта {remain:>10.2f} грн")
+# купівля
+def buy(currency):
+  result_buy['auh_value'] = float(input(f"Введіть кількість гривень, яку ви хочете обміняти на {currency}: "))
+  result_buy['currency_value'] = result_buy['auh_value'] // AUH_BUY[currency]
+  result_buy['remain'] = result_buy['auh_value'] % AUH_BUY[currency]
+  bank_val()
+  if(result_buy['enough_money']):
+    print(f"Ваша валюта {result_buy['currency_value']:>9.2f} {currency}")
+    print(f"Ваша решта {result_buy['remain']:>10.2f} UAH")
+    offer()
+    if(result_buy['offer_more']):
+      input_buy_more = input(f'Чи бажаете внести { result_buy["amount_more"] } грн щоб отримати рівно {result_buy["result_buy_more"]} дол?(Y/N):')
+    elif(result_buy['offer_less']):
+      pass
+    else:
+      print('Гарного дня!')
+  # Не вситачає грошей
+  else:
+    input_buy = input(f'В обміннику не вистачає грошей. Можемо запропонувати {result_buy["bank_sum"]} {currency}?(Т/Н)')
+    if input_buy == 'Y' or input_buy == 'y' or input_buy == 'Т' or input_buy == 'т':
+      result_buy['remain'] = result_buy['auh_value'] - (result_buy["bank_sum"] / AUH_BUY[currency])
+      print(f"Ваша валюта {result_buy['bank_sum']:>9.2f} {currency}")
+      print(f"Ваша решта {result_buy['remain']:>10.2f} UAH")
+    elif input_buy == 'N' or input_buy == 'n' or input_buy == 'Н' or input_buy == 'н':
+      print('Гарного дня!')
+    else:
+      print('Помилка вводу')
   
-  remain_usd = remain / AUH_BUY['USD']
-  auto_index_more = ALIQUIT_INDEX - (buy_calculations['result'] % ALIQUIT_INDEX) - remain_usd
-  auto_index_less = buy_calculations['result'] % ALIQUIT_INDEX
-
-  if buy_calculations['result'] % ALIQUIT_INDEX == 0:
-    print('Гарного дня!')
-  elif auto_index_more <= closeness_index:
-    result_buy_more = buy_calculations['result'] + auto_index_more + remain_usd
-    surcharge_amount = auto_index_more * AUH_BUY['USD']
-    input_buy_more = input(f'Чи бажаете внести { surcharge_amount } грн щоб отримати рівно {result_buy_more} дол?(Y/N):')
-    
-    if input_buy_more == 'Y' or input_buy_more == 'y':
-      print(f'Внесіть суму до плати: { surcharge_amount } грн')
-      print(f"Ваша валюта {result_buy_more:.2f} дол")
-      print(f"Ваша решта 0 грн")
-    elif input_buy_more == 'N' or input_buy_more == 'n':
-      print(f"Ваша валюта {buy_calculations['result']:>9.2f} дол")
-      print(f"Ваша решта {remain:>10.2f} грн")
-    else:
-      print('Помилка вводу')
-  elif auto_index_less <= closeness_index: 
-    additional_buy = buy_calculations['result'] - auto_index_less
-    input_buy_less = input(f'Чи бажаєте купити рівно {additional_buy} дол?(Y/N):')
-    if input_buy_less == 'Y' or input_buy_less == 'y':
-      usd_remain = auto_index_less * AUH_BUY['USD'] + remain
-      print(f"Ваша валюта {additional_buy:>9.2f} дол")
-      print(f"Ваша решта {usd_remain:>10.2f} грн")
-    elif input_buy_less == 'N' or input_buy_less == 'n':
-      print(f"Ваша валюта {buy_calculations['result']:>9.2f} дол")
-      print(f"Ваша решта {remain:>10.2f} грн")
-    else:
-      print('Помилка вводу')
+  
+# продаж
+def sell(currency):
+  value = float(input(f"Введіть кількість {currency}, яку ви хочете продати: "))
+  result_sell['value'] = value *AUH_CELL[currency]
 
 if operation_selection == 'купити' or operation_selection == 'regbnb':
-  buy()
-  # value = float(input("Введіть кількість гривень, яку ви хочете продати: "))
-  # result = value // AUH_BUY['USD']
-  # remain = value % AUH_BUY['USD']
-  # print(f"Ваша валюта {result:>9.2f} дол")
-  # print(f"Ваша решта {remain:>10.2f} грн")
-  
-  # remain_usd = remain / AUH_BUY['USD']
-  # auto_index_more = ALIQUIT_INDEX - (result % ALIQUIT_INDEX) - remain_usd
-  # auto_index_less = result % ALIQUIT_INDEX
-
-  # if result % ALIQUIT_INDEX == 0:
-  #   print('Гарного дня!')
-  # elif auto_index_more <= closeness_index:
-  #   result_buy_more = result + auto_index_more + remain_usd
-  #   surcharge_amount = auto_index_more * AUH_BUY['USD']
-  #   input_buy_more = input(f'Чи бажаете внести { surcharge_amount } грн щоб отримати рівно {result_buy_more} дол?(Y/N):')
-    
-  #   if input_buy_more == 'Y' or input_buy_more == 'y':
-  #     print(f'Внесіть суму до плати: { surcharge_amount } грн')
-  #     print(f"Ваша валюта {result_buy_more:.2f} дол")
-  #     print(f"Ваша решта 0 грн")
-  #   elif input_buy_more == 'N' or input_buy_more == 'n':
-  #     print(f"Ваша валюта {result:>9.2f} дол")
-  #     print(f"Ваша решта {remain:>10.2f} грн")
-  #   else:
-  #     print('Помилка вводу')
-  # elif auto_index_less <= closeness_index: 
-  #   input_buy_less = input(f'Чи бажаєте купити рівно {result - (auto_index_less)} дол?(Y/N):')
-  #   if input_buy_less == 'Y' or input_buy_less == 'y':
-  #     usd_remain = auto_index_less * AUH_BUY['USD'] + remain
-  #     print(f"Ваша валюта {result - auto_index_less:>9.2f} дол")
-  #     print(f"Ваша решта {usd_remain:>10.2f} грн")
-  #   elif input_buy_less == 'N' or input_buy_less == 'n':
-  #     print(f"Ваша валюта {result:>9.2f} дол")
-  #     print(f"Ваша решта {remain:>10.2f} грн")
-  #   else:
-  #     print('Помилка вводу')
-
-  
+  buy(result_buy['currency']) 
 elif operation_selection == 'продати' or operation_selection == 'ghjlfnb':
-  value = float(input("Введіть кількість доларів, яку ви хочете продати: "))
-  result = value * AUH_CELL['USD']
-  print(f"Ви отримуєте {result:>8.2f} грн")
+  sell(result_sell['currency'])
+  print(f"Ви отримуєте {result_sell['value']:>8.2f} грн")
 else:
   print('Помилка вводу')
+
+
+
+
+# def buy():
+  
+  
+
+
+#     elif auto_index_more <= closeness_index:
+#       result_buy_more = buy_calculations['result'] + auto_index_more + remain_usd
+#       surcharge_amount = auto_index_more * AUH_BUY['USD']
+#       input_buy_more = input(f'Чи бажаете внести { surcharge_amount } грн щоб отримати рівно {result_buy_more} дол?(Y/N):')
+    
+#       if input_buy_more == 'Y' or input_buy_more == 'y':
+#         print(f'Внесіть суму до плати: { surcharge_amount } грн')
+#         print(f"Ваша валюта {result_buy_more:.2f} дол")
+#         print(f"Ваша решта 0 грн")
+#       elif input_buy_more == 'N' or input_buy_more == 'n':
+#         print(f"Ваша валюта {buy_calculations['result']:>9.2f} дол")
+#         print(f"Ваша решта {remain:>10.2f} грн")
+#       else:
+#         print('Помилка вводу')
+#     elif auto_index_less <= closeness_index: 
+#       additional_buy = buy_calculations['result'] - auto_index_less
+#       input_buy_less = input(f'Чи бажаєте купити рівно {additional_buy} дол?(Y/N):')
+#       if input_buy_less == 'Y' or input_buy_less == 'y':
+#         usd_remain = auto_index_less * AUH_BUY['USD'] + remain
+#         print(f"Ваша валюта {additional_buy:>9.2f} дол")
+#         print(f"Ваша решта {usd_remain:>10.2f} грн")
+#       elif input_buy_less == 'N' or input_buy_less == 'n':
+#         print(f"Ваша валюта {buy_calculations['result']:>9.2f} дол")
+#         print(f"Ваша решта {remain:>10.2f} грн")
+#       else:
+#         print('Помилка вводу')
+#   else:
+#     print("Недостатньо грошей в касі")
+  
+  
+
+
 
 
 
